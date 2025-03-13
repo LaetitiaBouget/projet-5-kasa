ca9bc42eacda83279e937331fc4959077adec579
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-03-13 22:45:55 +0100

    routage 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-function App() {
+import { BrowserRouter as Router } from 'react-router-dom'
+import AppRouter from './components/AppRouter'
+import NotFound from './NotFound'
+import NavBar from './NavBar'
+import Banner from './Banner'
+import Footer from './Footer'
 
+const App = () => {
   return (
-    <>
-      <p>Coucou</p>
-    </>
+    <Router>
+      <NavBar />
+      <Banner />
+      <AppRouter />
+      <Footer />
+    </Router>
   )
 }
 
 export default App
+
--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -2,19 +2,10 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import PropertyDetails from './pages/PropertyDetails'
-import NotFound from './NotFound'
-import NavBar from './NavBar'
-import Banner from './Banner'
-import Footer from './Footer'
 import PropertyCard from './PropertyCard'
-
-
 
 const AppRouter = () => {
     <Router>
-        <NavBar />
-        <Banner />
-        <Footer />
         <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
