b95d83595f25d4d8d32e320d59ac925d3d046457
Laetitia Bouget <laetitia.bouget@yahoo.com>
2025-03-22 21:10:04 +0100

    style-page-erreur

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,11 @@
+import { Link } from "react-router-dom"
+
+
 function NotFound() {
-    return <div>
+    return <div className="not-found">
       <img src="src\assets\images\404.png" alt="Erreur 404"/>
+      <p className="not-found__content"> Oups! La page que vous demandez n'existe pas. </p>
+      <Link to="/" className="link__not-found">Retourner sur la page d'accueil</Link>
     </div>
   }
   
